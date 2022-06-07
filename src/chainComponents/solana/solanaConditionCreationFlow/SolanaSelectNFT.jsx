9fc5158c041d5372145bf897cc847576269aeebf
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-06-07 15:51:27 -0700

    add individual nft checking

--- src/chainComponents/solana/solanaConditionCreationFlow/SolanaSelectNFT.jsx
+++ src/chainComponents/solana/solanaConditionCreationFlow/SolanaSelectNFT.jsx
@@ -1,37 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 import { ShareModalContext } from "../../../shareModal/createShareContext.js";
 import LitFooter from "../../../reusableComponents/litFooter/LitFooter";
 import LitInput from "../../../reusableComponents/litInput/LitInput";
 import { utils } from "ethers";
 
-const SolanaSelectNFT = ({ setSelectPage, handleUpdateUnifiedAccessControlConditions }) => {
+const SolanaSelectNFT = ({
+  setSelectPage,
+  handleUpdateUnifiedAccessControlConditions,
+}) => {
   const { setDisplayedPage, flow } = useContext(ShareModalContext);
   const [contractAddress, setContractAddress] = useState("");
-  const [tokenId, setTokenId] = useState("");
   const [addressIsValid, setAddressIsValid] = useState(false);
 
   useEffect(() => {
-    const isValid = utils.isAddress(tokenId);
+    const isValid = utils.isAddress(contractAddress);
     setAddressIsValid(isValid);
-  }, [tokenId])
+  }, [contractAddress]);
 
   const handleSubmit = () => {
     const unifiedAccessControlConditions = [
       {
-        conditionType: 'solRpc',
-        method: "GetTokenAccountsByOwner",
-        params: [
-          ":userAddress",
-          {
-            programId: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
-          },
-          {
-            encoding: "jsonParsed",
-          },
-        ],
-        chain: 'solana',
+        conditionType: "solRpc",
+        method: "balanceOfToken",
+        params: [contractAddress],
+        chain: "solana",
         returnValueTest: {
-          key: `$[?(@.account.data.parsed.info.mint == "${contractAddress}")].account.data.parsed.info.tokenAmount.amount`,
+          key: "$.amount",
           comparator: ">",
           value: "0",
         },
@@ -39,33 +33,27 @@ const SolanaSelectNFT = ({ setSelectPage, handleUpdateUnifiedAccessControlCondit
     ];
 
     handleUpdateUnifiedAccessControlConditions(unifiedAccessControlConditions);
-    setSelectPage('chooseAccess');
+    setSelectPage("chooseAccess");
 
-    if (flow === 'singleCondition') {
-      setDisplayedPage('review');
-    } else if (flow === 'multipleConditions') {
-      setDisplayedPage('multiple');
+    if (flow === "singleCondition") {
+      setDisplayedPage("review");
+    } else if (flow === "multipleConditions") {
+      setDisplayedPage("multiple");
     }
   };
 
   return (
-    <div className={'lsm-condition-container'}>
-      <h3 className={'lsm-condition-prompt-text'}>Which wallet
-        should be able to access this asset?</h3>
-        <h3 className={'lsm-condition-prompt-text'}>Select
-          token or
-          enter contract address</h3>
-        <LitInput value={contractAddress}
-                  setValue={setContractAddress}
-        />
-        <h3 className={'lsm-condition-prompt-text'}>
-          Add Token ID</h3>
-        <LitInput value={tokenId}
-                  setValue={setTokenId}
-        />
-      <LitFooter backAction={() => setSelectPage('chooseAccess')}
-                 nextAction={handleSubmit}
-                 nextDisableConditions={(!tokenId.length || !contractAddress.length)}/>
+    <div className={"lsm-condition-container"}>
+      <h3 className={"lsm-condition-prompt-text"}>
+        Which wallet should be able to access this asset?
+      </h3>
+      <h3 className={"lsm-condition-prompt-text"}>Enter the token address</h3>
+      <LitInput value={contractAddress} setValue={setContractAddress} />
+      <LitFooter
+        backAction={() => setSelectPage("chooseAccess")}
+        nextAction={handleSubmit}
+        nextDisableConditions={!contractAddress.length}
+      />
     </div>
   );
 };
